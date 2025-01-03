a3abb956f7f43d862877e4701fa606a1ef098a9c
Sunilyogi333 <sunilnath0109@gmail.com>
2025-01-03 15:19:32 +0545

    Add IoT controller and routes for LED and pump control via WebSocket

--- backend/src/routes/iot.routes.js
+++ backend/src/routes/iot.routes.js
@@ -1,6 +1,11 @@
-import { Router } from "express";
-import { verifyUser } from "../middlewares/auth.middleware.js";
+// src/routes/iot.routes.js
+import express from 'express';
+import { led, pump } from '../controllers/iot.controller.js';
 
-const router= Router()
+const router = express.Router();
+
+// Endpoint to control the LED
+router.post('/control-led', led);
+router.post('/control-pump', pump)
 
 export default router;
